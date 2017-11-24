995f3159c2d787dc599ed4513a51543bb3600923
J. Alexander <xandermoorman@gmail.com>
2017-11-24 13:48:04 -0600

    Extract lnk and prefetch files.

---
+++ ExtractLnkFiles.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import re
+import sys
+
+"""
+Program filters timeliner to only include created files and updated files. 
+"""
+
+import_file = sys.argv[1]
+export_file = sys.argv[2]
+
+if sys.argv != 2:
+    print("You need to supply two arguments for this program to work. The import filename and export filename.")
+    sys.exit(1)
+
+df = pd.DataFrame()
+
+df = df.from_csv(import_file, sep=',', index_col='Date')
+
+pattern = r'\.lnk|\.pf'
+regex1 = re.compile(pattern, flags=re.IGNORECASE)
+df['mask'] = df[['File Name']].apply(lambda x: x.str.contains(regex1, regex=True)).any(axis=1)
+df = df[df['mask'] == True]
+pattern2 = r'm|b'
+regex2 = re.compile(pattern2, flags=re.IGNORECASE)
+
+df['mask2'] = df[['Type']].apply(lambda x: x.str.contains(regex2, regex=True)).any(axis=1)
+df = df[df['mask2'] == True]
+df.drop(['mask', 'mask2'], axis=1, inplace=True)
+
+
+df.to_csv(export_file, index=True)
