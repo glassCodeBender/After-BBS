6975e69c267d79e7658ba2b9e7bc78f14b5bc630
J. Alexander <xandermoorman@gmail.com>
2017-11-24 14:16:07 -0600

    The list is still pretty short.

--- ExtractImportantExtensions.py
+++ ExtractImportantExtensions.py
@@ -10,14 +10,25 @@ USAGE: ~$ python ExtractImportantExtensions.py file_to_read.csv file_to_write.cs
 import_file = sys.argv[1]
 export_file = sys.argv[2]
 
+if sys.argv != 2:
+    print("You need to supply two arguments for this program to work. The import filename and export filename.")
+    sys.exit(1)
+
 df = pd.DataFrame()
 
 df = df.from_csv(import_file, sep=',', index_col='Date')
 
-pattern = r'\.exe|\.rar|\.sys|\.jar|\.pf|\.lnk|\.bat'
+# other extensions that might be useful.
+other_stuff = '\.pdf|\.ain|\.dbg|\.torrent\.ts'
+
+programming_stuff = '\.cbl|\.class|\.cob|\.cs$|\.cxx\.def\.di\.erl\.f$|\.m$|\.p$|\.pli|\.sym'
+
+pattern = r'\.exe|\.rar|\.bi|\.ps|\.sys|\.jar|\.bcp|\.ctl|\.ctt|\.b|\.pf|\.rb|\.s$|\.swift|\.vbs +' \
+          r'|\.rs$|\.lnk|\.bat|compiler|tmp|\.c$|\.vb|\.cc|\.cp|\.scala|\.pl|.asm|\.sh|\.bash'
 regex1 = re.compile(pattern, flags=re.IGNORECASE)
 df['mask'] = df[['File Name']].apply(lambda x: x.str.contains(regex1, regex=True)).any(axis=1)
 df = df[df['mask'] == True]
+
 pattern2 = r'm|b'
 regex2 = re.compile(pattern2, flags=re.IGNORECASE)
 
@@ -25,5 +36,4 @@ df['mask2'] = df[['Type']].apply(lambda x: x.str.contains(regex2, regex=True)).a
 df = df[df['mask2'] == True]
 df.drop(['mask', 'mask2'], axis=1, inplace=True)
 
-new_filename = 'by_ext_' + 'test.csv'
 df.to_csv(export_file, index=True)
